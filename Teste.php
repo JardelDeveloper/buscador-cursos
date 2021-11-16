3769a751360cc00662c725ba135b4fe5f8d6d39f
Jardel Cunha <developerprojectfullstack@gmail.com>
2021-11-16 19:47:43 -0300

    Migrate the APP for Github
    App Features developed:
    -Searches the name of all courses in a section of the Alura website;
    -It performs three types of tests in the application, they are PHPUnit (Unit Test), PHPCS (Design Patterns) and PHAN (Static Analysis of code syntax).

---
+++ Teste.php
@@ -0,0 +1,17 @@
+<?php
+
+class Test
+{
+    public static function method()
+    {
+        echo "Test";
+    }
+}
+
+class Test2
+{
+    public static function method()
+    {
+        echo "Test2";
+    }
+}
